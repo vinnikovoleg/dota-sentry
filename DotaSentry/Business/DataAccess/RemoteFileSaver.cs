8e6fb84945019b484170ffadcf0ae6bfbd492de6
Vinnikov Oleh <oleg.vinnikov@creuna.com>
2020-06-09 21:41:05 +0300

    Update Dashboard UI Update Live match API

---
+++ DotaSentry/Business/DataAccess/RemoteFileSaver.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DotaSentry.Business.DataAccess
+{
+    public class RemoteFileSaver
+    {
+        public async Task<string> SaveAsync(string url, string filePath)
+        {
+            using var http = new HttpClient();
+            using var data = await http.GetAsync(url);
+            await using var file = File.Create(filePath);
+            await data.Content.CopyToAsync(file);
+            return filePath;
+        }
+    }
+}
