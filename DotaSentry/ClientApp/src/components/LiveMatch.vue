d7b6958fe4d54ecde5ad573c6c385bda63be1b5c
Vinnikov Oleh <oleg.vinnikov@creuna.com>
2020-06-18 22:50:35 +0300

    Update dashboard styles

--- DotaSentry/ClientApp/src/components/LiveMatch.vue
+++ DotaSentry/ClientApp/src/components/LiveMatch.vue
@@ -1,20 +1,29 @@
 <template>
-  <div
+  <router-link
     class="match"
+    :to="{ name: 'LiveMatch', params: { serverSteamId: model.serverSteamId }}"
   >
-    <router-link
-      class="team"
-      :to="{ name: 'LiveMatch', params: { serverSteamId: model.serverSteamId }}"
-    >
-      <LiveTeam :model="model.radiant" />
-    </router-link>
-    <router-link
-      class="team"
-      :to="{ name: 'LiveMatch', params: { serverSteamId: model.serverSteamId }}"
-    >
-      <LiveTeam :model="model.dire" />
-    </router-link>
-  </div>
+    <LiveTeam :model="model.radiant" />
+
+    <div class="score-container">
+      <div class="score">
+        <span>{{ model.radiant.score }}</span>
+        <span>{{ model.dire.score }} </span>
+      </div>
+      <div class="lead">
+        <div>
+          <span v-if="model.radiant.lead > 0">{{ model.radiant.lead }}</span>
+        </div>
+        <div>
+          <span v-if="model.dire.lead > 0">
+            {{ model.dire.lead }}
+          </span>
+        </div>
+      </div>
+    </div>
+
+    <LiveTeam :model="model.dire" />
+  </router-link>
 </template>
 
 <script>
