46647edc84302960f0a35d04353bc2956173b113
Vinnikov Oleh <oleg.vinnikov@creuna.com>
2020-06-29 22:14:17 +0300

    Add  LiveMatchStoreRepository.cs

---
+++ DotaSentry/Business/DataAccess/LiveMatchStoreRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using DotaSentry.Models;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace DotaSentry.Business.DataAccess
+{
+    public class LiveMatchStoreRepository
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public LiveMatchStoreRepository(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public List<LiveMatch> Get()
+        {
+            return _memoryCache.Get<List<LiveMatch>>(LiveMatchStoreUpdateJob.MatchCacheKey);
+        }
+    }
+}
